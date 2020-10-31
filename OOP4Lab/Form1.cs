bf3352cb482f77c39f0c5ced53e856172e3d0c43
javantlang <javantlang@gmail.com>
2020-11-01 00:04:15 +0500

    Добавил возможность возвращаться к предыдущему объекту. Добавил комментарии

--- OOP4Lab/Form1.cs
+++ OOP4Lab/Form1.cs
@@ -12,39 +12,53 @@ namespace OOP4Lab
 {
     public partial class PaintBox : Form
     {
+        //Создаём список
         LinkedList Mylist;
+        //Создаём объект класса graphics для рисования
         private Graphics g;
+        //Буфер для bitmap изображения
         private Bitmap bitmapDraw;
         public PaintBox()
         {
             InitializeComponent();
+            //Инициализируем список
             Mylist = new LinkedList();
+            //Инициализируем объект bitmap, копируем размер drawBox в него
             bitmapDraw = new Bitmap(drawBox.Width, drawBox.Height);
+            //Инициализация g
             g = Graphics.FromImage(bitmapDraw);
+            g.Clear(Color.White);
         }
 
         public bool controlPressed()
         {
+            //Зажат ли control
             return (Control.ModifierKeys & Keys.Control) == Keys.Control;
         }
 
         public void Draw()
         {
+            //Очищает 
             g.Clear(Color.White);
+            //Рисуем все круги
             Mylist.front();
             while (!Mylist.eol())
             {
+                //Если данный объект выделен, то он будет закрашен
                 if (Mylist.getObject().Current == true)
                     Mylist.getObject().FillDraw(bitmapDraw);
+                //иначе не будет закрашен
                 else
                     Mylist.getObject().Draw(bitmapDraw);
                 Mylist.next();
             }
+            //Копируем изображение из буфера в drawBox
             drawBox.Image = bitmapDraw;
         }
 
         public bool inCircle(int xPos, int yPos)
         {
+            //Если control не зажат, объекты перестанут быть текущими
             if (!controlPressed())
             {
                 Mylist.front();
@@ -54,6 +68,7 @@ namespace OOP4Lab
                     Mylist.next();
                 }
             }
+            //проверка, находился ли курсор при нажатии в круге
             Mylist.front();
             while (!Mylist.eol())
             {
@@ -66,6 +81,8 @@ namespace OOP4Lab
                     Mylist.next();
                 }
             }
+            //Если control был зажат, но объекты не были выделены, то
+            //создасться новый объект, а остальные перестанут быть выделеными
             Mylist.front();
             while (!Mylist.eol())
             {
@@ -77,6 +94,8 @@ namespace OOP4Lab
 
         private void circleCreate(object sender, MouseEventArgs e)
         {
+            //если курсор не был в уже созданном круге, создасться уже новый круг
+            //И изображение обновится
             if (!inCircle(e.X, e.Y))
             {
                 Mylist.push_back(new CCircle(e.X, e.Y));
@@ -88,10 +107,13 @@ namespace OOP4Lab
             }
         }
 
+        //Событие при нажатии клавиши
         private void Paint_KeyDown(object sender, KeyEventArgs e)
         {
+            //Проверяет, нажата ли клавиша Delete
             if (e.KeyCode == Keys.Delete)
             {
+                //Удаляет все объекты со значение current = true
                 Mylist.front();
                 while (!Mylist.eol())
                 {
@@ -102,6 +124,7 @@ namespace OOP4Lab
                     else
                         Mylist.next();
                 }
+                //Вырисовывает
                 Draw();
             }
         }
--- OOP4Lab/LinkedList.cs
+++ OOP4Lab/LinkedList.cs
@@ -11,25 +11,36 @@ namespace OOP4Lab
 {
     class LinkedList
     {
+        //Начало списка
         Node root;
+        //Конец списка
         Node tail;
+        //Текущий элемент
         Node current;
+        //Счётчик элементов
         private int count = 0;
 
+        //Конструктор по умолчанию
         public LinkedList()
         {
             root = null;
             tail = root;
             current = root;
         }
+        //Получение размера списка
         public int size
         {
             get => count;
         }
+        //Пустой ли список
         public bool isEmpty() { return root == null; }
+        //Является ли текущий элемент концом списка
         public bool eol() { return current == null; }
+        //Устанавливает текущий элемент в начало списка
         public void front() { current = root; }
+        //Устанавливает текущий элемент в конец списка
         public void back() { current = tail; }
+        //Устанавливает текущее значение на следующее
         public void next()
         {
             if (current.nextNode != null)
@@ -37,6 +48,15 @@ namespace OOP4Lab
             else
                 current = null;
         }
+        //Устанавливает текущее значение на предыдущее
+        public void prev()
+        {
+            if (current.prevNode != null)
+                current = current.prevNode;
+            else
+                current = null;
+        }
+        //Добавляет объект в начало списка
         public void push_front(Shape newObj)
         {
             ++count;
@@ -54,6 +74,7 @@ namespace OOP4Lab
             }
             current = root;
         }
+        //Добавляет объект в конец списка
         public void push_back(Shape newObj)
         {
             ++count;
@@ -70,6 +91,7 @@ namespace OOP4Lab
                 tail = newNode;
             }
         }
+        //Вставляет объект после it
         public void insert(Node it, Shape newObj)
         {
             ++count;
@@ -83,6 +105,7 @@ namespace OOP4Lab
             else
                 tail = newNode;
         }
+        //Удаляет объект it
         public void erase(Node it)
         {
             --count;
@@ -108,6 +131,7 @@ namespace OOP4Lab
             }
             it = null;
         }
+        //Полностью очищает список
         public void clear()
         {
             if (root == null)
@@ -121,18 +145,22 @@ namespace OOP4Lab
             } while (delRoot != null);
             count = 0;
         }
+        //Возвращает текущий объект, хранящийся в Node
         public Shape getObject()
         {
             return current.getObj;
         }
-        
+        //Возвращает текущий объект Node
         public Node getCurrent()
         {
             return current;
         }
     }
+
+    //Узел для хранения объектов в списке
     class Node
     {
+        //Объект Shape для доступа к методам
         private Shape obj;
         private Node next;
         private Node prev;
@@ -157,7 +185,15 @@ namespace OOP4Lab
             get => obj;
             set => obj = value;
         }
+        ~Node()
+        {
+            nextNode = null;
+            prevNode = null;
+            obj = null;
+        }
     }
+
+    //Абстрактный класс для хранения различных объектов
     abstract class Shape
     {
         public abstract bool Current { get; set; }
